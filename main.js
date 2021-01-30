56f1ba0dcf4d8082e2bc03ce4e6286d3cf97f636
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-01-30 10:54:39 -0600

    added data structure and ranhouse function

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log('connected');
+
+const students = [
+  {
+    name: 'Neville',
+    id: 23,
+    house: ''
+  },
+  {
+    name: 'Harry',
+    id: 19,
+    house: ''
+  },
+  {
+    name: 'Ron',
+    id: 35,
+    house: ''
+  },
+];
+
+const printToDom = (divId, printedString) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerhtml = printedString;
+};
+// Function to choose a random house
+const ranHouse = () => {
+  const ranNum = (Math.floor(Math.random() * 4) + 1);
+  let house = '';
+
+  switch(ranNum) {
+    case 1:
+      house = 'Slytherin';
+      break;
+    case 2:
+      house = 'Gryffindor';
+      break;
+    case 3:
+      house = 'Hufflepuff';
+      break;
+    case 4:
+      house = 'Ravenclaw';
+      break;
+  }
+  return house;
+};
