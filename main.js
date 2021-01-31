4365e024c04fb92f071754f3f53dccc699ca9b72
Chie Stroud <urirunrunkoko@gmail.com>
2021-01-30 18:36:25 -0600

    added delete function

--- main.js
+++ main.js
@@ -11,13 +11,29 @@ const students = [
   {
     id: 1,
     name: 'Harry Potter',
-    house: 'Gryffindor'
+    house: 'Griffindor'
   },
   {
     id: 2,
     name: 'Hermione Granger',
     house: 'Griffindor'
   },
+  {
+    id: 3,
+    name: 'Nymphadora Tonks',
+    house: 'Hufflepuff'
+  },
+  {
+    id: 4,
+    name: 'Luna Lovegood',
+    house: 'Ravenclaw'
+  },
+  {
+    id: 5,
+    name: 'Grahm Pitchard',
+    house: 'Slytherin'
+  },
+
 ]
 
 
@@ -29,7 +45,7 @@ const printToDom = (divId, textToPrint) => {
 const studentBuilder = (taco) => {
   let showOnDom = '';
   taco.forEach((item, i) => {
-    showOnDom += `<div class="card" style="width: 18rem;" id=${i}>
+    showOnDom += `<div class="card ${item.house}" style="width: 18rem;" id=${i}>
                     <div class="card-body text-center">
                       <h5 class="card-title">${item.name}</h5>
                       <p class="card-text">${item.house}</p>
@@ -51,16 +67,28 @@ const getStudentInfo = (e) => {
     id,
   }
   students.push(obj);
-  console.log(students);
   studentBuilder(students);
   document.querySelector('form').reset();
-  
+  // reset method restores a form element's default value.
 }
 
+
+// Expelling student and push it into a different array.
+const expelStudent = (e) => {
+  const targetType = e.target.type;
+  const targetId = e.target.id;
+  const expelledStudent = [];
+  if (targetType === 'button') {
+    expelledStudent.push(students.splice(targetId, 1));
+  }
+  // console.log(expelledStudent); confirmed it is giving me back the array.
+  studentBuilder(students);
+}
 
 
 const handleButtonClick = (e) => {
   const buttonId = e.target.id;
+  //If I console log buttonId, it gives me the object in the array
   
   if (buttonId === 'showForm') {
     document.querySelector('#hideForm').style.display = 'block';
@@ -70,11 +98,13 @@ const handleButtonClick = (e) => {
 const buttonEvents = () => {
   document.querySelector('#showForm').addEventListener('click', handleButtonClick);
   document.querySelector('form').addEventListener('submit', getStudentInfo);
+  document.querySelector('#studentCard').addEventListener('click', expelStudent)
 }
 
 const init = () => {
   buttonEvents();
   studentBuilder(students);
 }
+
 
 init();
