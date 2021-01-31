311e7d65eddcf7199e9f3cfb1b2b5e388d2ecc81
Mitchell Crumbley <mitchellcrumbley@gmail.com>
2021-01-31 02:44:25 -0600

    added the error message functionality for empty attempts

--- main.js
+++ main.js
@@ -14,10 +14,7 @@ const printToDom = (divId, textToPrint) => {
 
 function showForm() {
   document.getElementById("myForm").style.display = "block";
-}; // sets the myForm div display type to block upon clicking the Get Started button.
-
-
-
+  }; // sets the myForm div display type to block upon clicking the Get Started button.
 
 
 const sortingHat = () => {
@@ -27,7 +24,6 @@ const sortingHat = () => {
 };
 
 
-
 //C in CRUD: Create New Students
 // Takes the name put into the student submission form and pushes it into the array.
 const addStudentToHouse = (e) => {
@@ -35,6 +31,7 @@ const addStudentToHouse = (e) => {
   let domString = '';
   if (e.target.id === "sort") { //If the target of an event is the button id 'sort' set the constant student = to the query on studentName
     const student = document.querySelector("#studentName");
+    errorMessage(student.value);
 
     if (student.value) { //If the value of student returns true then push the name and schoolHouse into the array.
       students.push({
@@ -46,45 +43,97 @@ const addStudentToHouse = (e) => {
   }
   studentBuilder(students); //run studentBuilder function to generate the card for the newly added student in the array. AKA - Rebiuld the DOM.
 
-  document.querySelector('form').reset(); //Reset the form after the button is clicked
+  document.querySelector('form').reset(); 
+  //Reset the form after the button is clicked
 };
-
-
-
-
-
 
 // //Creates a pie container card on the DOM
 const studentBuilder = (taco) => {
   let domString = '';
-  // for (const [i, item] of taco.entries()) {
-  //   const {name, schoolHouse} = item;
-  // CANT GET FOR EACH OR FOR OF LOOPS TO WORK. Error states they are not functions.
 
-
-  //FOR LOOP ISNT ALLOWED IN THIS ASSIGNMENT. This Generates
-  for (let i = 0; i < taco.length; i++) {
+  //FOR OF LOOP - This works!
+  for (const [i, item] of taco.entries()) {
+    const { name, schoolHouse } = item;
     domString += `<div class="card" style="width: 18rem;" id=${i}>
-                    <div class="card-body">
-                      <p class="card-text" id="studentName">${taco[i].name}</p>
-                      <p class="card-text"id="schoolHouse">${taco[i].schoolHouse}</p>
-                      <button type="button" id="${i}" class="btn btn-danger">EXPEL</button>
-                    </div>
-                  </div>`;
-  };
+    <div class="card-body">
+      <p class="card-text" id="studentName">${name}</p>
+      <p class="card-text"id="schoolHouse">${schoolHouse}</p>
+      <button type="button" id="${i}" class="btn btn-danger">EXPEL</button>
+    </div>
+  </div>`;
+  }
   printToDom('#first-years-card-display', domString);
 };
+//OTHER OPTIONS!!
+
+// FOR EACH LOOP - This works!
+// taco.forEach((item, i) => {
+//   domString += `<div class="card" style="width: 18rem;" id=${i}>
+//   <div class="card-body">
+//     <p class="card-text" id="studentName">${taco[i].name}</p>
+//     <p class="card-text"id="schoolHouse">${taco[i].schoolHouse}</p>
+//     <button type="button" id="${i}" class="btn btn-danger">EXPEL</button>
+//   </div>
+// </div>`;
+// });
+// printToDom('#first-years-card-display', domString);
+// };
+
+//FOR LOOP ISNT ALLOWED IN THIS ASSIGNMENT. This Generates
+//   for (let i = 0; i < taco.length; i++) {
+//     domString += `<div class="card" style="width: 18rem;" id=${i}>
+//                     <div class="card-body">
+//                       <p class="card-text" id="studentName">${taco[i].name}</p>
+//                       <p class="card-text"id="schoolHouse">${taco[i].schoolHouse}</p>
+//                       <button type="button" id="${i}" class="btn btn-danger">EXPEL</button>
+//                     </div>
+//                   </div>`;
+//   };
+//   printToDom('#first-years-card-display', domString);
+// };
+
+// const errorMessage = (student) => {
+//   if (student) {
+//       document.querySelector("#error-message").innerHTML = "";
+// } else {
+//     document.querySelector("#error-message").innerHTML = 
+//         `<div style="color: rgb(226, 13, 16);margin-bottom: 10px;">
+//         <b>Don't be shy. Speak a Name!</b>
+//       </div>`;
+//   }
+// };
+
+// function showError() {
+//   if (student = true) {
+//     document.getElementById("error-message").style.display = "block";
+//   } else {
+//     document.getElementById("error-message").style.display = "none";
+//    };
+// };
+
+const errorMessage = (student) => {
+  if (student) {  //If student has a value when passed to the function set the div's innerHTML to nothing.
+      document.querySelector("#error-message").innerHTML = "";
+} else { //If student returns false when passed set div's innerHTML = to the syled div we setup on the HTML page.
+    document.querySelector("#error-message").innerHTML = 
+        `<div style="color: red;margin-bottom: 10px;">
+        <b>Don't be shy! Say your name.</b>
+      </div>`;
+  }
+};
+
 
 const buttonEvents = () => {
   document.querySelector("#get-started").addEventListener("click", showForm);
   document.querySelector("#sort").addEventListener("click", addStudentToHouse);
+  // document.querySelector("#sort").addEventListener('click', showError);
 };
 
 
 const init = () => {
   studentBuilder(students);
   buttonEvents();
-    console.log(students);
+  console.log(students);
 };
 
 init();
