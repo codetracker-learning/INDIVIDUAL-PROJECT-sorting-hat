c3d1ba369bc912b72535cfdf893cf1931d8372af
Nathan Martin <nathanmartin5937@gmail.com>
2021-02-02 11:25:42 -0600

    creating form to dom

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-console.log("CONNECTED");
+const students = [
+    {
+        name: 'Ryan',
+        groupHouse: 'Hufflepuff',
+        Age: 18,
+        imgurl: 'https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg'
+    },
+    {
+        name: 'Andrew',
+        groupHouse: 'Gryffindor',
+        Age: 20,
+        imgurl: 'https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg'
+    },
+    {
+        name: 'Kaily',
+        groupHouse: 'Ravenclaw',
+        Age: 21,
+        imgurl: 'https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg'
+    },
+    {
+        name: 'Nathan',
+        groupHouse: 'Slytherin',
+        Age: 20,
+        imgurl: 'https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg'
+    } 
+];
+
+const groupHouses = [
+    {
+        name: 'Hufflepuff',
+        imgurl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
+    },
+    {
+        name: 'Gryffindor',
+        imgurl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
+    },
+    {
+        name: 'Ravenclaw',
+        imgurl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
+    },
+    {
+        name: 'Slytherin',
+        imgurl: 'https://wizardingwonders.com/wp-content/uploads/harry-potter-hogwarts-house-flag-banner.jpg'
+    } 
+];
+
+function appearForm = () {
+    document.querySelector
+}
+
