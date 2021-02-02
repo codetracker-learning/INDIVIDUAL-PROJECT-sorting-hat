fed1087a01ca9b0c1feb9dc54fbfc5cfc97b946d
Nathan Martin <nathanmartin5937@gmail.com>
2021-02-02 12:55:45 -0600

    printing to dom

--- main.js
+++ main.js
@@ -44,7 +44,40 @@ const groupHouses = [
     } 
 ];
 
-function appearForm = () {
-    document.querySelector
+const renderForm = (content, form) => {
+    const selectedForm = document.querySelector(form);
+    selectedForm.innerHTML = content;
+  };
+
+const appearForm = () => {
+    const content = `<form id="infoForm">
+                        <div class="form-name">
+                        <label for="inputName">Please enter your name:</label>
+                        <input type="name" class="form-control" id="inputName" aria-describedby="nameHelp"      placeholder="EnterName">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>`
+
+renderForm('#infoForm', content); 
+};
+
+const handlebtnClick = (e) => {
+    const btnId = e.target.id;
+  
+   
+    if (btnId === 'getStarted') {
+      document.querySelector('form').style.display;
+    } else {
+        console.log("Please click get started");
+    }
 }
 
+const btnEvents = () => {
+    document.querySelector("form").addEventListener("getStarted", renderForm);
+}
+
+const init = () => {
+    btnEvents();
+  }
+  
+  init();
