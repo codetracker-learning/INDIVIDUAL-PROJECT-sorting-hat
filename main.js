d953dfdbb48b2ba1049dadb900922c2f2970f0e9
Jim Administrator <jamesdavidconner@gamil.com>
2021-02-01 22:00:01 -0600

    added commit at end of night

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-console.log("connected");
+console.log("connected")
 
 const showForm = () => {
   document.getElementById('formElement').style.display ='block';
 }
+
+
+
+const randomHouse = [
+  "Gryffindor",
+  "Hufflepuff",
+  "Ravenclaw",
+  "Slytherin"
+];
+// add printToDom here
+
+
