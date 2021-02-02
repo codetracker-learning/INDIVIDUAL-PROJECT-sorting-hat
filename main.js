5686540dbe407e1607ea47bed7ebef4551c89d2c
Sean <jb@JBs-MacBook-Pro.local>
2021-02-01 19:52:48 -0600

    added working sort button

--- main.js
+++ main.js
@@ -1,55 +1,62 @@
-let studentArray = [
-  {
-    firstName: "Sean",
-    lastName: "Rossettie",
-    school: "Hufflepuff",
-  }
+const students =[
 ];
 
-let form =`<form>
-<div class="mb-3">
-  <label for="exampleInputEmail1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-</div>
-</form>`;
+const houses = [ 
+  "Gryffindor",
+  "Hufflepuff",
+  "Ravenclaw",
+  "Slytherin",
+]
 
-const printToDom = (divId, studentString) => {
+const printToDom = (divId, string) => {
   const selectedDiv = document.querySelector(divId);
-  selectedDiv.innerHTML = studentString;
+  selectedDiv.innerHTML = string;
 }
 
-const studentMaker = (studentArr) => {
-  let cardString = '';
+// const studentMaker = (studentArr) => {
+//   let cardString = '';
 
-  for (let [i, element] of studentArr.entries()) {
-    cardString += `<div class="card" style="width: 18rem;" id="${i}">
-    <div class="card-body">
-      <h5 class="card-title">${element.firstName} ${element.lastName}</h5>
-      <p class="card-text">${element.school}</p>
-      <button type="button" class="btn btn-danger">Expel</button>
-    </div>
-  </div>`;
-  };
+//   studentArr.forEach((item, i)) {
+//     cardString += `<div class="card" style="width: 18rem;" id="${i}">
+//     <div class="card-body">
+//       <h5 class="card-title">${studentArr[i].name}</h5>
+//       <p class="card-text">${studentArr[i].school}</p>
+//       <button type="button" class="btn btn-danger" id=${studentArr[i].id}>Expel</button>
+//     </div>
+//   </div>`;
+//   };
   
-  printToDom("#students", cardString);
+//   printToDom("#students", cardString);
+
+//   students.push()
+// }
+
+const createForm = (e) => {
+  let formId = e.target.id;
+
+  if (formId === "sort") {
+    document.querySelector("#form").innerHTML=
+    `<form>
+      <div class="mb-3">
+        <label for="exampleStudentInput" class="form-label">New Student Name</label>
+        <input type="name" class="form-control" id="studentName" aria-describedby="nameInput">
+        <div id="nameInput" class="form-text">Find you school!.</div>
+      </div>
+    </form>`
+    }
+  document.querySelector("#studentName").
 }
 
-const printForm = (divId, formToPrint) => {
-  const formField = document.querySelector(divId);
-  formField.innerHTML = formToPrint;
-  
-}
 
 
 const buttonEvents = () => {
-  document.querySelector('#sort').addEventListener('click', printForm);
+  document.querySelector('#sort').addEventListener('click', createForm);
 }
 
 
 const init = () => {
-  printForm("#form", form);
-  studentMaker(studentArray);
+  buttonEvents();
+  // studentMaker(students);
 }
 
 init();
