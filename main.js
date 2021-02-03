3cb2511e2d7051792f94559db1b941e753014727
Nathan Keith <nashvegasnate@gmail.com>
2021-02-02 20:22:01 -0600

    fixed all previous issues

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 console.log("CONNECTED");
+
+const studentArray = [];
+
+  const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.querySelector(divId);
+    selectedDiv.innerHTML = textToPrint;
+  }
+
+const createStudentCards = () => {
+  let domString = '';
+  studentArray.forEach((item, i) => {
+    domString += `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">${item.studentName}</h5>
+                      <p class="card-text">${item.house}</p>
+                      <a href="#" class="btn btn-primary">Expel</a>
+                    </div>;
+                  </div>`
+  })
+ 
+  printToDom('#studentCards', domString);
+}
+
+const handleButtonClick = (e) => {
+const studentName = document.querySelector("#studentName").value;
+  const newStudent = {
+    studentName,
+    house: "Slytherin",
+  }
+
+  studentArray.push(newStudent);
+  createStudentCards();
+}
+
+const buttonEvents = () => {
+  document.querySelector("#sortBtn").addEventListener('click', handleButtonClick);
+}
+
+
+const init = () => {
+    buttonEvents();
+};
+
+init();
