939107c05f388f3e6ac7ef88915a6c9de393fc27
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-02-02 20:54:41 -0600

    Added lots of logic to our JS. Created multiple functions.

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-console.log("Ravenclaw or GTFO")
+// console.log("Ravenclaw or GTFO")
+
+const studentsArr = [];
+
+
+
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId)
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const createStudentsCards = (taco) => {
+  let domString = '';
+  studentsArr.forEach((item, i) => {
+    domString += `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${item.studentName}</h5>
+    <p class="card-text">${item.house}</p>
+    <a href="#" class="btn btn-primary">Expel!</a>
+  </div>
+</div>`
+  })
+  printToDom('#studentCards', domString);
+
+}
+
+const handleButtonClick = (e) => {
+  e.preventDefault();
+  const studentName = document.querySelector('#studentName').value;
+  const newStudent = {
+    studentName,
+    house: 'Slytherin',
+  }
+
+  studentsArr.push(newStudent);
+  createStudentsCards();
+}
+
+const buttonEvents = () => {
+  document.querySelector('#sortBtn').addEventListener('click', handleButtonClick);
+  
+}
+
+const init = () => {
+  buttonEvents();
+};
+
+
+init();
